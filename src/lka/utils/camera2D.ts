56695a0235b2b9e060f8271a8e2300e277bc4c58
LairLiu <liukunair@gmail.com>
2018-01-30 15:53:37 +0800

    ADD TODOList

---
+++ src/lka/utils/camera2D.ts
@@ -0,0 +1,13 @@
+module lka {
+    /**
+     * 2D摄像机
+     * 用于将镜头焦点固定在移动的物体上
+     * 参考：http://bbs.egret.com/thread-43544-1-1.html
+     * 
+     * @export
+     * @class camera
+     */
+    export class camera {
+
+    }
+}
